34f654b5b7f81e551dd14fab54909ec06b3e8037
Henry <hleejr0103@gmail.com>
2020-05-13 18:48:55 -0700

    templating engine established, user can login

--- manga/views.py
+++ manga/views.py
@@ -2,17 +2,10 @@ import os, csv
 from django.shortcuts import render
 from django.http import HttpResponse
 from manga_monster import settings
-
+from .models import Manga
 # Create your views here.
-def index(request):
-
-    with open(os.path.join(settings.BASE_DIR, 'manga', 'resources', 'manga_data.csv')) as f:
-        reader = csv.reader(f)
-        next(reader, None)
-        manga = 0
-        count = 0
-
-        for row in reader:
-            manga += 1    
-
-    return HttpResponse('There are {} manga in this database'.format(manga), 200)
+def index(request): 
+    context = {
+        'manga': Manga.objects.all()
+    }
+    return render(request, 'manga/index.html', context)
